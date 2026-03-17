d9603eb2ac8f4741edb3c2f123de711e0168c213
Carolina Marcos Cárdaba <carolmarcosc@gmail.com>
2026-03-17 14:11:17 +0100

    ex00

---
+++ ex00/Megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	std::string str;
+
+	if (ac == 1)
+		str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; av[i]; i++)
+		{
+			for (int j = 0; av[i][j]; j++)
+			{
+				str.push_back(toupper(av[i][j]));
+			}
+		}
+	}
+	std::cout << str << std::endl;
+	return 0;
+}
