a8807d80abe0f35cfedfedc531e7da16860049f4
Carolina Marcos Cárdaba <carolmarcosc@gmail.com>
2026-03-18 17:50:18 +0100

    final

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -11,39 +11,50 @@ Contact::Contact()
 	this->DarkSecret = "";
 }
 
-Contact::Contact(std::string name, std::string lastName, std::string nickName, std::string phoneNumber, std::string darkSecret) {
-	this->Name = name;
-	this->LastName = lastName;
-	this->NickName = nickName;
-	this->PhoneNumber = phoneNumber;
-	this->DarkSecret = darkSecret;
+Contact::Contact(std::string Name, std::string LastName, std::string NickName, std::string PhoneNumber, std::string DarkSecret)
+{
+	this->Name = Name;
+	this->LastName = LastName;
+	this->NickName = NickName;
+	this->PhoneNumber = PhoneNumber;
+	this->DarkSecret = DarkSecret;
 }
 
 Contact::~Contact() {}
 
 std::string	Contact::getName(bool truncate)
 {
-	return truncate ? this->truncate(this->Name) : this->Name;
+	if (truncate)
+		return this->truncate(this->Name);
+	return this->Name;
 }
 
 std::string	Contact::getLastName(bool truncate)
 {
-	return truncate ? this->truncate(this->LastName) : this->LastName;
+	if (truncate)
+		return this->truncate(this->LastName);
+	return this->LastName;
 }
 
 std::string	Contact::getNickName(bool truncate)
 {
-	return truncate ? this->truncate(this->NickName) : this->NickName;
+	if (truncate)
+		return this->truncate(this->NickName);
+	return this->NickName;
 }
 
 std::string	Contact::getPhoneNumber(bool truncate)
 {
-	return truncate ? this->truncate(this->PhoneNumber) : this->PhoneNumber;
+	if (truncate)
+		return this->truncate(this->PhoneNumber);
+	return this->PhoneNumber;
 }
 
 std::string	Contact::getDarkSecret(bool truncate)
 {
-	return truncate ? this->truncate(this->DarkSecret) : this->DarkSecret;
+	if (truncate)
+		return this->truncate(this->DarkSecret);
+	return this->DarkSecret;
 }
 
 std::string	Contact::truncate(std::string str)
