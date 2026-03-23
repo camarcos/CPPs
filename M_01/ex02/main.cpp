02696feb74e6e1994f775259cff9b9da9e7894df
Carolina Marcos Cárdaba <carolmarcosc@gmail.com>
2026-03-23 17:42:52 +0100

    update

--- M_01/ex02/main.cpp
+++ M_01/ex02/main.cpp
@@ -5,26 +5,29 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: carolinamc <carolinamc@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/22 20:07:33 by carolinamc        #+#    #+#             */
-/*   Updated: 2026/03/23 16:45:48 by carolinamc       ###   ########.fr       */
+/*   Created: 2026/03/23 17:31:18 by carolinamc        #+#    #+#             */
+/*   Updated: 2026/03/23 17:33:17 by carolinamc       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include <iostream>
 
-int main( void )
+int	main( void )
 {
-	randomChump("Carla");
 
-	Zombie zombie1("Lola");
-	Zombie zombie2 = Zombie("Marta");
-	Zombie *zombie3 = newZombie("Pepa");
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
 
-	zombie1.announce();
-	zombie2.announce();
-	zombie3->announce();
+	/* PRINT VALUES */
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 
-	delete zombie3;
+	/* PRINT MEMORY ADDRESS */
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
 
 	return 0;
 }
