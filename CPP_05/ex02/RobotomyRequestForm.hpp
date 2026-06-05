f8a1bedcc9564cc5d3fe67e05bc4c4605a8f1ff4
Carolina Marcos Cárdaba <carolmarcosc@gmail.com>
2026-06-05 09:52:17 +0200

    mas cositas

--- CPP_05/ex02/RobotomyRequestForm.hpp
+++ CPP_05/ex02/RobotomyRequestForm.hpp
@@ -6,8 +6,29 @@
 /*   By: carolinamc <carolinamc@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 12:41:27 by carolinamc        #+#    #+#             */
-/*   Updated: 2026/06/04 12:42:14 by carolinamc       ###   ########.fr       */
+/*   Updated: 2026/06/04 15:32:02 by carolinamc       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include "AForm.hpp"
+
+#define SIGN_ROBOTOMY 72
+#define EXEC_ROBOTOMY 45
+
+class RobotomyRequestForm : public AForm
+{
+	public:
+		/* OCCF */
+		RobotomyRequestForm();
+		RobotomyRequestForm( const std::string _target );
+		RobotomyRequestForm( const RobotomyRequestForm& other );
+		RobotomyRequestForm& operator=( const RobotomyRequestForm& other );
+		~RobotomyRequestForm();
+
+		/* Public Methods */
+		virtual void execute(Bureaucrat const & executor) const;
+};
--- CPP_05/ex02/ShrubberyCreationForm.hpp
+++ CPP_05/ex02/ShrubberyCreationForm.hpp
@@ -6,7 +6,35 @@
 /*   By: carolinamc <carolinamc@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 12:41:21 by carolinamc        #+#    #+#             */
-/*   Updated: 2026/06/04 12:41:22 by carolinamc       ###   ########.fr       */
+/*   Updated: 2026/06/04 15:00:08 by carolinamc       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include "AForm.hpp"
+
+#define SIGN_SHRUBBERY 145
+#define EXEC_SHRUBBERY 137
+
+class ShrubberyCreationForm : public AForm
+{
+	private:
+		/* Exceptions */
+		class FileNotOpenException : public std::exception
+		{
+			const char* what() const throw();
+		};
+	public:
+		/* OCCF */
+		ShrubberyCreationForm();
+		ShrubberyCreationForm( const std::string _target );
+		ShrubberyCreationForm( const ShrubberyCreationForm& other );
+		ShrubberyCreationForm& operator=( const ShrubberyCreationForm& other );
+		~ShrubberyCreationForm();
+
+		/* Public Methods */
+		virtual void execute(Bureaucrat const & executor) const; // override
+};
